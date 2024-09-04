2f08f999cef84a29587c7493ec80ae3ca4074c4c
richard.millett <richard.millett+github@googlemail.com>
2024-09-04 16:56:12 +0100

    moved connection credentials to environment variables

--- libraries/data/database-connection.ts
+++ libraries/data/database-connection.ts
@@ -1,15 +1,18 @@
 import mysql, { Pool, PoolConnection } from 'mysql2/promise';
+import { EnvironmentVariables } from '../environment-variables';
 
 class MySQLConnections {
   static #instance: MySQLConnections;
   readonly pool: Pool;
 
   private constructor() {
+    let env = EnvironmentVariables.instance;
+
     this.pool = mysql.createPool({
-      connectionLimit : 10,
-      user: 'snipeit_user',
-      database: 'snipeit',
-      password: 'snipeit_password'
+      connectionLimit : env.dbMaxConnectionsPerPool,
+      user: env.dbUsername,
+      database: env.dbName,
+      password: env.dbPassword
     });
   }
 
--- libraries/environment-variables.ts
+++ libraries/environment-variables.ts
@@ -6,12 +6,21 @@ export class EnvironmentVariables {
     public readonly baseUrl: string;
     public readonly username: string;
     public readonly password: string;
+    public readonly dbName: string;
+    public readonly dbUsername: string;
+    public readonly dbPassword: string;
+    public readonly dbMaxConnectionsPerPool: number;
 
     private constructor() {
         dotenv.config()
         this.baseUrl = process.env.BASE_URL ?? '';
         this.username = process.env.SITE_USERNAME ?? '';
         this.password = process.env.SITE_PASSWORD ?? '';
+
+        this.dbName = process.env.DB_NAME ?? '';
+        this.dbUsername = process.env.DB_USERNAME ?? '';
+        this.dbPassword = process.env.DB_PASSWORD ?? '';
+        this.dbMaxConnectionsPerPool = parseInt(process.env.DB_MAX_CONNECTIONS_PER_POOL ?? "5");
     }
 
     public static get instance(): EnvironmentVariables{
