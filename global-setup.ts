7457e8c16bf53d805b93cb24ef890ffa686788af
richard.millett <richard.millett+github@googlemail.com>
2024-09-11 17:14:25 +0100

    ./ is best practice for specifying relative paths

--- global-setup.ts
+++ global-setup.ts
@@ -6,7 +6,7 @@ const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 async function globalSetup(config: FullConfig) {
     console.log('Start global setup');
     execSync('podman-compose down');
-    execSync('setup_teardown_scripts/setup.sh')
+    execSync('./setup_teardown_scripts/setup.sh')
     execSync('podman-compose up -d');
     await delay(30000); // TODO: remove by fixing the setup delay
     console.log('End global setup');
