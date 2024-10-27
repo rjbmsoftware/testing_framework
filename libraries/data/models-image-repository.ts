10e7183b1defe6dd86e11a8da0eb3b67c719d927
richard.millett <richard.millett+github@googlemail.com>
2024-10-27 10:41:42 +0000

    I can now get the file from snipe it and store in a temporary directory

---
+++ libraries/data/models-image-repository.ts
@@ -0,0 +1,29 @@
+import { EnvironmentVariables } from "../environment-variables";
+import axios from "axios";
+import fs from "node:fs";
+import path from "node:path";
+
+export class ModelsImageRepository {
+    readonly url: string;
+
+    constructor() {
+        this.url = EnvironmentVariables.instance.baseUrl + '/uploads/models'
+    }
+
+    /**
+     * Downloads the file from uploads models storing the file in a temporary
+     * directory
+     * @param imagePath suffix to download URL 
+     * @returns path to file
+     */
+    async getImage(imagePath: string): Promise<string> {
+        const imageUrl = `${this.url}/${imagePath}`;
+        const imageFilePath = path.join('.', 'temporary-files', imagePath);
+
+        await axios.get(imageUrl, {responseType: 'stream'}).then(function (response) {
+            response.data.pipe(fs.createWriteStream(imageFilePath));
+        });
+
+        return imageFilePath;
+    }
+}
--- tests/settings/asset-models.spec.ts
+++ tests/settings/asset-models.spec.ts
@@ -5,11 +5,13 @@ import { CategoriesRepository } from "../../libraries/data/categories-repository
 import { CreateAssetModelPage } from "../../pages/settings/asset-models-create-page";
 import { AssetModelsRepository } from "../../libraries/data/asset-models-repository";
 import { MySQLConnections } from "../../libraries/data/database-connection";
+import { ModelsImageRepository } from "../../libraries/data/models-image-repository";
 
 const test = base.extend<{
     createAssetModelPage: CreateAssetModelPage,
     categoriesRepository: CategoriesRepository,
-    assetModelsRepository: AssetModelsRepository
+    assetModelsRepository: AssetModelsRepository,
+    modelsImageRepository: ModelsImageRepository
 }>({
     createAssetModelPage: async ({ page }, use) => {
         const createAssetModelPage = new CreateAssetModelPage(page);
@@ -23,31 +25,43 @@ const test = base.extend<{
     },
 
 
-    assetModelsRepository: async({}, use) => {
+    assetModelsRepository: async ({ }, use) => {
         // TODO: refactor into base test
         const instance = MySQLConnections.getInstance();
         const assetModelsRepository = new AssetModelsRepository(instance);
         await use(assetModelsRepository);
+    },
+
+    modelsImageRepository: async ({ }, use) => {
+        const modelsImageRepository = new ModelsImageRepository();
+        await use(modelsImageRepository);
     }
 });
 
-test("create asset model", async ({ 
+test("create asset model", async ({
     createAssetModelPage,
     categoriesRepository,
-    assetModelsRepository
- }) => {
+    assetModelsRepository,
+    modelsImageRepository
+}) => {
+
     const categoryName = await categoriesRepository.createCategory();
     const assetModelName = uuid();
     const assetModelPage = await createAssetModelPage.createAssetModel(
         assetModelName, categoryName, getComputerImageFixturePath()
     );
 
-
     expect(assetModelPage.isAssetModelsPage()).toBeTruthy();
+
+    // assert asset model is matches uploaded image, starting to look like two tests
+    // --get the asset model
+    // --from the asset model find the image
+    // download the image and compare
     const assetModel = await assetModelsRepository.findByName(assetModelName);
     expect(assetModel).toBeTruthy();
-    // assert asset model is matches uploaded image
-        // get the asset model
-        // from the asset model find the image
-        // download the image and compare
+
+    if (assetModel && assetModel.image) {
+        const imageFilePath = modelsImageRepository.getImage(assetModel.image);
+    }
+
 });
