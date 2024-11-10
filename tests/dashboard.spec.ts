951ca686b3b081e6d5a550bb2e4562e2150be36b
richard.millett <richard.millett+github@googlemail.com>
2024-11-10 11:49:16 +0000

    backstop js is a separate framework, I will generate the screenshots from playwright

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -13,6 +13,20 @@ test('dashboard title test', async ({ dashboardPage }) => {
   await expect(dashboardPage.page).toHaveTitle(/Dashboard :: Mega IT Corporate 9000!/);
 });
 
+/**
+ * Uses playwright which underneath uses pixel match for pixel perfect comparison, this can changed
+ * by setting the allowable different pixel count
+ *
+ * to update screenshots
+ * npx playwright test --update-snapshots
+ */
 test('dashboard playwright screen shot', async ({ dashboardPage }) => {
   await expect(dashboardPage.page).toHaveScreenshot();
+});
+
+/**
+ * Generate reference and actual screenshots for comparison by backstop js
+ */
+test('visually compare menu', async ({ dashboardPage }) => {
+  // TODO 
 });
